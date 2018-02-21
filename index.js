b84900013f724423b19c8c4a330aea8ec6d8be9a
Shawn T <shawn@huthunt.com>
2018-02-21 01:25:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 function BoardMember(name, homeState, training) {
-  this.name = name
-  this.homeState = homeState
-  this.training = training
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+  this.veto
+  function() {
+    return 'No, I must disagree'
+  }
+  this.approve
+  function() {
+    return 'You can do that!'
+  }
+  this.doCharity
+  function() {
+    return 'I like to help people.'
+  }
+  this.releasePressStatement
+  function() {
+    return 'You will see great things from Scuber'
+  }
 
-function veto() {
-  return 'No, I must disagree'
-}
-
-function approve() {
-  return 'You can do that!'
-}
-function doCharity() {
-  return 'I like to help people.'
-}
-
-function releasePressStatement() {
-  return 'You will see great things from Scuber'
-}
-
-function sayHi() {
-  return 'Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.'
-}
+  this.sayHi
+  function() {
+    return 'Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.'
+  }
 }
